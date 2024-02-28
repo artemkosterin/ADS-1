49a2210111370679a6c7d3ea8542509b07f1b1f0
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-02-28 18:09:18 +0300

    b117 Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -16,15 +16,15 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-    if (n == 1) return 2; 
+    if (n == 1) return 2;
     uint64_t count = 1;
-    uint64_t number = 3; 
+    uint64_t number = 3;
     while (count < n) {
         if (checkPrime(number)) {
             count++;
         }
         if (count < n) {
-            number += 2; 
+            number += 2;
         }
     }
     return number;
@@ -43,8 +43,7 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t summ = 0;
     for (uint64_t i = 2; i < hbound; i++) {
-        if (checkPrime(i))
-        {
+        if (checkPrime(i)) {
             summ += i;
         }
     }
